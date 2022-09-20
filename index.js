86f66236e30725b9028d15a2b74ee858a187fa4f
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2022-09-20 10:55:13 -0400

    commit tree 4e6f9234b4aef699082eb86c402bef68fe55f31b

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  //iterate through the array with a for loop
+  for (let i = 0; i < array.length; i++) {
+  //for current num, identify compliment that adds to target(comp=target-num)
+  const complement = target - array[i];
+  //iterate through the rest of the array
+  for (let j = i + 1; j < array.length; j++) {
+  //check if any number is our complement
+    if(array[j] === complement) return true;
+  }
+  }
+  //if I reach teh end of the array, return false
+  return false;
 }
 
 /* 
@@ -8,6 +19,15 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  hasTargetSum([22, 19, 4, 6, 30], 25);
+// returns true, since 19 and 6 add up to 25
+iterate through each number of this array.
+  for the current number, identify a compliment that adds to the target (comp = target - num)
+  iterate through the reet of the array
+  check if any number is our compliment
+  if so, return true
+
+  if I get to the end of the array, and nothing adds up correctly, return false
 */
 
 /*
